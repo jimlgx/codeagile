1c8363ad9dcce1caf274bec0e67fe318d0859339
jimlgx <jimlgx@gmail.com>
2013-07-19 18:29:12 +0800

    Signed-off-by: jimlgx <jimlgx@gmail.com>

---
+++ stepbystep/src/test/java/org/apache/maven/archetype/generator/Test.java
@@ -0,0 +1,24 @@
+package org.apache.maven.archetype.generator;
+
+/*
+ * @project jimlgx-codeagile-stepbystep
+ * @package 
+ * @file Test.java
+ * @version  1.0
+ * @author  wangjunming
+ * @time  2013-7-19
+ */
+
+/**
+ * <code>Test</code>
+ * 
+ * @version 1.0
+ * @author wangjunming
+ * @since 1.0 2013-7-19
+ */
+public class Test {
+
+	org.codehaus.plexus.classworlds.launcher.Launcher launcher;
+	
+//	org.apache.maven.cli.MavenCli 
+}
