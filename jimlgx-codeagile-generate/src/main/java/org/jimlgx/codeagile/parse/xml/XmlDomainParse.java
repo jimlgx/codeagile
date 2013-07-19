220c91ee564e3005c59911e249b06cb025e63e68
jimlgx <jimlgx@gmail.com>
2013-07-19 23:17:24 +0800

    Signed-off-by: jimlgx <jimlgx@gmail.com>

---
+++ jimlgx-codeagile-generate/src/main/java/org/jimlgx/codeagile/parse/xml/XmlDomainParse.java
@@ -0,0 +1,46 @@
+/*
+ * @project jimlgx-codeagile-generate
+ * @package org.jimlgx.codeagile.parse.xml
+ * @file XmlDomainParse.java
+ * @version  1.0
+ * @author  wangjunming
+ * @time  2013-7-19
+ */
+package org.jimlgx.codeagile.parse.xml;
+
+import java.util.List;
+
+import org.jimlgx.codeagile.generate.model.DomainModel;
+import org.jimlgx.codeagile.parse.DomainParse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <code>XmlDomainParse</code>
+ * 
+ * @version 1.0
+ * @author wangjunming
+ * @since 1.0 2013-7-19
+ */
+public class XmlDomainParse implements DomainParse {
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+
+	/**
+	 * 
+	 */
+	public XmlDomainParse() {
+		super();
+	}
+
+	/**
+	 * <code>parseDomains</code>
+	 * 
+	 * @return
+	 * @since 2013-7-19 wangjunming
+	 */
+	public List<DomainModel> parseDomains() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
